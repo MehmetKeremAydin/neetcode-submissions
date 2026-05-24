4565bb5b8693bd7d586c1c54d134239f09e0ff2d
Mehmet Kerem Aydın <80589280+MehmetKeremAydin@users.noreply.github.com>
2026-05-24 15:05:59 -0500

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        def recSearch(i:int, curSubset:list) -> None:
+            if i == len(nums):
+                answer.append(curSubset.copy())
+                return
+            for j in range(numCnts[i]+1):
+                if j>0:
+                    curSubset.append(nums[i])
+                recSearch(i+1, curSubset)
+            for j in range(numCnts[i]):
+                curSubset.pop()
+            return
+        
+        answer = []
+        subset = []
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        nums, numCnts = list(freq.keys()), list(freq.values())
+        recSearch(0, subset)
+        return answer
+        
+
+
+
+        
