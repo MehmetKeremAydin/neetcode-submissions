3afee73cf56b4e68c954eec0ae0a0dd2ab4fd877
Mehmet Kerem Aydın <80589280+MehmetKeremAydin@users.noreply.github.com>
2026-05-24 13:53:17 -0500

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        answer = nums[0]
+        for i in range(1, len(nums)):
+            answer = answer ^ nums[i]
+        return answer
+        
