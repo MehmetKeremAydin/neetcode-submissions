e94e21935ef78b6b0987073147a2f48c12aa4ee9
Mehmet Kerem Aydın <80589280+MehmetKeremAydin@users.noreply.github.com>
2026-06-01 19:25:07 -0500

    Add: min-cost-climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        def recursiveCost(n):
+            if n in alreadySeen:
+                return alreadySeen[n]
+            if n >= lenStairs:
+                return 0
+            cost_a = recursiveCost(n+1)
+            cost_b = recursiveCost(n+2)
+            alreadySeen[n] = min(cost_a, cost_b) + cost[n]
+            return alreadySeen[n]
+            
+        lenStairs = len(cost)
+        alreadySeen = {}
+        return min(recursiveCost(0), recursiveCost(1))
