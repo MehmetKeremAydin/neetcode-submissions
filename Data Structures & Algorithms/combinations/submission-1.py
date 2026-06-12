1ee9482b6ee27b862cff1a203f50ffefd27d6165
Mehmet Kerem Aydın <80589280+MehmetKeremAydin@users.noreply.github.com>
2026-06-11 18:15:17 -0600

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def recSearch(i, curComb):
+            if len(curComb) == k:
+                answer.append(curComb)
+                return
+            if i > n:
+                return
+            recSearch(i+1, curComb.copy())
+            curComb.append(i)
+            recSearch(i+1, curComb.copy())
+
+        answer = []
+        curComb = []
+        recSearch(1, curComb)
+        return answer
+        
