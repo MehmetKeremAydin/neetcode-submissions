c51488df18ef94bd2dca714d82c3f2288031b008
Mehmet Kerem Aydın <80589280+MehmetKeremAydin@users.noreply.github.com>
2026-06-16 12:52:11 -0600

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        def recursiveSearch(baseText:str, longText:str) -> int:
+            if (baseText, longText) in memory:
+                return memory[(baseText, longText)]
+            if baseText == "" or longText == "":
+                return 0
+            idx = longText.find(baseText[0])
+            result1 = 0
+            if idx != -1:
+                result1 = recursiveSearch(baseText[1:], longText[(idx+1):]) + 1
+            result2 = recursiveSearch(baseText[1:], longText)
+            result = max(result1, result2)
+            memory[(baseText, longText)] = result
+            return result
+        
+        
+        memory = {}
+        return recursiveSearch(text1, text2)
