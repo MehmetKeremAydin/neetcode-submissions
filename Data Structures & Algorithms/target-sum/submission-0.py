e2a73220b591c7e019a058d43e9bdd2694ce92df
Mehmet Kerem Aydın <80589280+MehmetKeremAydin@users.noreply.github.com>
2026-06-16 17:25:30 -0600

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        def recursiveSearch(i, target):
+            if i == len(nums): 
+                if target == 0:
+                    return 1
+                else:
+                    return 0
+            count1 = recursiveSearch(i+1, target + nums[i])
+            count2 = recursiveSearch(i+1, target - nums[i])
+            return count1 + count2
+        
+        memory = {}
+        return recursiveSearch(0, target)
