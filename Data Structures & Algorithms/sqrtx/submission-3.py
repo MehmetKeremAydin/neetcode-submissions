8b5d66c461da55aa166151fa9b4d8d913b6056d9
Mehmet Kerem Aydın <80589280+MehmetKeremAydin@users.noreply.github.com>
2026-06-21 02:17:05 -0600

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+        while l<=r:
+            c = (l + r) // 2
+            sq = c*c
+            if sq == x:
+                return c
+            elif sq < x:
+                l = c + 1
+            else:
+                r = c - 1
+        return l-1
+        
