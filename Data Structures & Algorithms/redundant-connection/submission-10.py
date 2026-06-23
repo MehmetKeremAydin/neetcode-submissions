fdcc44a2f399e66cb1741e324061615fb250b5b2
Mehmet Kerem Aydın <80589280+MehmetKeremAydin@users.noreply.github.com>
2026-06-22 22:00:24 -0600

    Add: redundant-connection - submission-10

---
+++ Data Structures & Algorithms/redundant-connection/submission-10.py
@@ -0,0 +1,38 @@
+class UnionFind():
+    def __init__(self, n:int):
+        self.parents = [i for i in range(n)]
+        self.rank = [1 for _ in range(n)]
+        self.numNodes = n
+
+    def find(self, i:int):
+        while self.parents[i] != i:
+            self.parents[i] = self.parents[self.parents[i]]
+            i = self.parents[i]
+        return i
+
+
+    def union(self, i:int, j:int):
+        pi, pj = self.find(i), self.find(j)
+        if pi == pj:
+            return False
+        if self.rank[pi] > self.rank[pj]:
+            self.parents[pj] = pi
+            self.rank[pi] += self.rank[pj]
+        else:
+            self.parents[pi] = pj
+            self.rank[pj] += self.rank[pi]
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        nodes = set()
+        for e in edges:
+            nodes.add(e[0])
+            nodes.add(e[1])
+        dsu = UnionFind(len(nodes))
+        for e in edges:
+            node1, node2 = e
+            if not dsu.union(node1-1, node2-1):
+                return e
+        return -1
+        
