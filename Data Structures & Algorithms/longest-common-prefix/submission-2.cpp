6a019a6a9be1bc4751fc9cabd1c76a684b8346e4
Mehmet Kerem Aydın <80589280+MehmetKeremAydin@users.noreply.github.com>
2026-08-18 00:47:19 -0500

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 1) return strs[0];
+        int n = strs[0].size();
+        for (int i = 0; i < strs[0].size(); i++) {
+            for (int j = 1; j < strs.size(); j++) {
+                if (strs[0][i] != strs[j][i]) return strs[0].substr(0, i);
+            }
+        }
+        return strs[0];
+    }
+};
